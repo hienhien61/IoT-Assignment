51978e25e9a0c5f7a1ddb6976c9f858b6a726104
Trần Hà Tuấn Kiệt <35926768+TuanKietTran@users.noreply.github.com>
2023-01-04 21:27:12 +0700

    Update main.py

--- publisher/main.py
+++ publisher/main.py
@@ -69,12 +69,12 @@ def publish(client):
         for x in data:
           msg = f"{json.dumps(x)}"
           result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
+          # result: [0, 1]
+          status = result[0]
+          if status == 0:
+              print(f"Send `{msg}` to topic `{topic}`")
+          else:
+              print(f"Failed to send message to topic {topic}")
         msg_count += 1
 
 
